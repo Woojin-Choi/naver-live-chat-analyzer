7201ffdfe055c097a00e48377b8b404be5b83d9d
Woojin-Choi <panda1996@snu.ac.kr>
2022-10-12 12:09:41 +0900

    Feat: add frequent word info

--- src/App.js
+++ src/App.js
@@ -18,6 +18,19 @@ function App() {
   const [videoLink, setVideoLink] = useState("");
   const [validRawChat, setValidRawChat] = useState([]);
   const [chatFrequency, setChatFrequency] = useState([]);
+  const [frequentWordInfoArr, setFrequentWordInfoArr] = useState([]);
+
+  // token frequency
+  const KoreanFirstableConsonant =
+    "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ".split("");
+  const KoreanMiddlableVowel =
+    "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ".split("");
+  const KoreanFinalableConsonant = [
+    undefined,
+    ..."ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ".split(""),
+  ];
+  const FirstSyllableForKorean = "가".charCodeAt();
+  const LastSyllableForKorean = "힣".charCodeAt();
 
   const onYoutubeLinkInputChange = (e) => {
     setVideoLink(e.target.value);
@@ -105,6 +118,24 @@ function App() {
         console.log("validRawChat", validRawChat);
         setValidRawChat(validRawChat);
 
+        let onlyCommentArr = [];
+        for (let rawChat of validRawChat) {
+          onlyCommentArr.push(rawChat.message);
+        }
+        console.log("onlyCommentArr", onlyCommentArr);
+
+        const allChatFrequentWord = rangeTokenCounter(onlyCommentArr);
+
+        const allChatFrequentWordArr = Object.entries(allChatFrequentWord).sort(
+          (a, b) => {
+            return b[1] - a[1];
+          }
+        );
+
+        setFrequentWordInfoArr(allChatFrequentWordArr);
+
+        console.log("allChatFrequentWordArr", allChatFrequentWordArr);
+
         let chattingFrequencyData = { x: [], y: [], formattedX: [] }; // x: interval sec, y: chat freq, formattedX: ex) 00:00:25
         for (
           let groupSec = 0;
@@ -147,6 +178,215 @@ function App() {
       console.log(`Error occurs while getting raw chat info: ${err}`);
       console.log(err);
     }
+  };
+
+  const isKoreanSyllable = (char) => {
+    // 음절인지?
+    const charCode = char.charCodeAt();
+    if (FirstSyllableForKorean <= charCode && charCode <= LastSyllableForKorean)
+      return true;
+    return false;
+  };
+
+  const decomposeSyllable = (syllable) => {
+    // 음절 분해
+    if (!isKoreanSyllable(syllable)) return syllable;
+
+    const syllableCode = syllable.charCodeAt();
+    return {
+      first:
+        KoreanFirstableConsonant[Math.floor((syllableCode - 0xac00) / 588)],
+      middle:
+        KoreanMiddlableVowel[Math.floor(((syllableCode - 0xac00) % 588) / 28)],
+      final:
+        KoreanFinalableConsonant[
+          Math.floor(((syllableCode - 0xac00) % 588) % 28)
+        ],
+    };
+  };
+
+  const arrangeSyllableToPhoneme = (letter) => {
+    // 음절을 음소로 나열
+    if (!isKoreanSyllable(letter)) return letter;
+
+    const disassedLetter = decomposeSyllable(letter);
+    return (
+      disassedLetter.first +
+      disassedLetter.middle +
+      (disassedLetter.final ? disassedLetter.final : "")
+    );
+  };
+
+  const resolveDoubleVowel = (char) => {
+    // 모음 분해
+    const table = {
+      ㅘ: "ㅗㅏ",
+      ㅙ: "ㅗㅐ",
+      ㅚ: "ㅗㅣ",
+      ㅝ: "ㅜㅓ",
+      ㅞ: "ㅜㅔ",
+      ㅟ: "ㅜㅣ",
+      ㅢ: "ㅡㅣ",
+    };
+    if (table[char] != undefined) return table[char];
+    return char;
+  };
+
+  const resolveDoubleConsonant = (char) => {
+    // 자음 분해
+    const table = {
+      ㄲ: "ㄱㄱ",
+      ㄳ: "ㄱㅅ",
+      ㄵ: "ㄴㅈ",
+      ㄶ: "ㄴㅎ",
+      ㄺ: "ㄹㄱ",
+      ㄻ: "ㄹㅁ",
+      ㄼ: "ㄹㅂ",
+      ㄽ: "ㄹㅅ",
+      ㄾ: "ㄹㅌ",
+      ㄿ: "ㄹㅍ",
+      ㅀ: "ㄹㅎ",
+      ㅄ: "ㅂㅅ",
+      ㅆ: "ㅅㅅ",
+    };
+    if (table[char] != undefined) return table[char];
+    return char;
+  };
+
+  const arrangeLineToPhoneme = (
+    line,
+    resolveConsonant = false,
+    resolveVowel = false,
+    joinSpace = false
+  ) => {
+    // 문장을 음소로 나열
+    let result = line
+      .split("")
+      .map((dl) => arrangeSyllableToPhoneme(dl))
+      .join("");
+    if (resolveVowel)
+      result = result
+        .split("")
+        .map((dl) => resolveDoubleVowel(dl))
+        .join("");
+    if (resolveConsonant)
+      result = result
+        .split("")
+        .map((dl) => resolveDoubleConsonant(dl))
+        .join("");
+    if (joinSpace) result = result.split(" ").join("");
+    return result;
+  };
+
+  const rangeTokenCounter = (
+    textArray,
+    frequentWordNum = Infinity,
+    exception = ["ㅋ"],
+    whenProjectOpen = true
+  ) => {
+    if (whenProjectOpen) {
+      console.log("start analyzing and downloading total chatting...");
+    }
+    const rateSame = 0.7;
+
+    let newTextArray = JSON.parse(JSON.stringify(textArray));
+    newTextArray.map((text, i) => {
+      exception.map((excep) => {
+        newTextArray[i] = text.split(excep).join(" ");
+      });
+    });
+    // console.log('newTextArray: ', newTextArray);
+
+    // let splitedTexts = [];
+    // newTextArray.map(text => (splitedTexts = [...splitedTexts, ...text.split(' ')]));
+
+    let splitedTexts = [];
+    for (let word of newTextArray) {
+      splitedTexts.push(...word.split(" "));
+    }
+
+    // console.log('splitedTexts', splitedTexts);
+
+    let frequency = {};
+    let similarTokens = {};
+    for (let tokenIdx in splitedTexts) {
+      let token = splitedTexts[tokenIdx];
+      let progressPercent = parseInt(
+        (Number(tokenIdx) / splitedTexts.length) * 100
+      );
+      if (
+        whenProjectOpen &&
+        parseInt((progressPercent - 1) / 10) !== parseInt(progressPercent / 10)
+      ) {
+        console.log("진행률", `${progressPercent} %`);
+      }
+      if (token.length === 0) continue;
+      if (frequency[token] !== undefined) {
+        frequency[token] += 1;
+        similarTokens[token].find((a) => a[0] === token)[1] += 1;
+      } else {
+        const arrangedToken = arrangeLineToPhoneme(token);
+        if (token.length === 1) {
+          frequency[token] = 1;
+          similarTokens[token] = [[token, 1]];
+          continue;
+        }
+
+        let found = "";
+        let existedTokens = Object.entries(frequency).map((a) => a[0]);
+        for (let i = 0; i < existedTokens.length; i++) {
+          const existed = existedTokens[i];
+          if (existed.length === 1) continue;
+
+          let arrangedExisted = arrangeLineToPhoneme(existed);
+          let numSame = 0;
+          let lenNew = arrangedToken.length;
+          let lenExisted = arrangedExisted.length;
+          for (let j = 0; j < Math.min(lenNew, lenExisted); j++) {
+            if (arrangedToken[j] === arrangedExisted[j]) numSame++;
+          }
+          if (numSame / Math.min(lenNew, lenExisted) >= rateSame) {
+            found = existed;
+            break;
+          }
+        }
+
+        if (found !== "") {
+          frequency[found] += 1;
+
+          if (similarTokens[found].findIndex((a) => a[0] === token) === -1) {
+            similarTokens[found].push([token, 1]);
+          } else {
+            similarTokens[found].find((a) => a[0] === token)[1] += 1;
+          }
+        } else {
+          frequency[token] = 1;
+          similarTokens[token] = [[token, 1]];
+        }
+      }
+    }
+
+    Object.entries(frequency).map((keyVal) => {
+      const key = keyVal[0];
+
+      similarTokens[key].sort((a, b) => b[1] - a[1]);
+      if (similarTokens[key][0][0] !== key) {
+        const newKey = similarTokens[key][0][0];
+        frequency[newKey] = frequency[key];
+        similarTokens[newKey] = similarTokens[key];
+        delete frequency[key];
+        delete similarTokens[key];
+      }
+    });
+    frequency = Object.fromEntries(
+      Object.entries(frequency)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, frequentWordNum)
+    );
+
+    // console.log({ textArray, frequency });
+
+    return frequency;
   };
 
   ChartJS.register(
@@ -206,7 +446,7 @@ function App() {
       <div style={{ height: "500px" }}>
         <Line options={options} data={data} />
       </div>
-      <div className="chatBoxContainer">
+      <div className="bottomContainer">
         <div className="chatBox">
           {validRawChat
             ? validRawChat.map((chat, idx) => {
@@ -221,6 +461,14 @@ function App() {
                 );
               })
             : null}
+        </div>
+        <div className="frequentWordBox">
+          {frequentWordInfoArr.map((info, idx) => (
+            <div className="frequentWordLine" key={`frequentWord${idx}`}>
+              <div>{info[0]}</div>
+              <div>{info[1]}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
